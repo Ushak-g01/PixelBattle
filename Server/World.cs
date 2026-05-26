7532ce4c0f6802b22c15ff1d0fc59d4c4454e6b0
Nikita60997 <nikitaoven201012@gmail.com>
2026-05-26 18:41:41 +0300

    Добавили проект сервера и класс мира

---
+++ Server/World.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class World
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public byte[,] Pixels { get; set; }
+        public List<string> Players { get; set; } = new List<string>();
+        public Dictionary<string, DateTime> LastPixeltime { get; set; } = new();
+        public Dictionary<string , int> PlayerScores { get; set; } = new();
+
+        public World (int id , string name , int width ,  int height)
+        {
+            Id = id;
+            Name = name;
+            Width = width;
+            Height = height;
+            Pixels = new byte[width,height];
+            for (int x = 0; x < width; x++)
+                for (int y = 0; y < height; y++)
+                    Pixels[x,y] = 0;
+        }
+    }
+}
