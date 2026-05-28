de76f2f7cbc2ac1a11bd9ac90a60bd434dc674c6
Xundertale <gaa2011@inbox.ru>
2026-05-28 18:08:20 +0300

    timer

--- ClientPB/Form1.Designer.cs
+++ ClientPB/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             label1 = new Label();
             btnConnect = new Button();
             TextServerIP = new TextBox();
@@ -37,16 +38,17 @@
             btnCreate = new Button();
             btnRefresh = new Button();
             panel1 = new Panel();
-            btnColor0 = new Button();
-            btnColor1 = new Button();
-            btnColor2 = new Button();
-            btnColor3 = new Button();
-            btnColor4 = new Button();
-            btnColor5 = new Button();
-            btnColor6 = new Button();
             btnColor7 = new Button();
+            btnColor6 = new Button();
+            btnColor5 = new Button();
+            btnColor4 = new Button();
+            btnColor3 = new Button();
+            btnColor2 = new Button();
+            btnColor1 = new Button();
+            btnColor0 = new Button();
             lblStatus = new Label();
             lblCooldown = new Label();
+            coldownTimer = new System.Windows.Forms.Timer(components);
             panel1.SuspendLayout();
             SuspendLayout();
             // 
@@ -140,51 +142,23 @@
             panel1.Size = new Size(330, 36);
             panel1.TabIndex = 8;
             // 
-            // btnColor0
+            // btnColor7
             // 
-            btnColor0.BackColor = Color.Black;
-            btnColor0.Location = new Point(3, 3);
-            btnColor0.Name = "btnColor0";
-            btnColor0.Size = new Size(35, 30);
-            btnColor0.TabIndex = 0;
-            btnColor0.UseVisualStyleBackColor = false;
-            btnColor0.Click += button1_Click;
+            btnColor7.BackColor = Color.Cyan;
+            btnColor7.Location = new Point(290, 3);
+            btnColor7.Name = "btnColor7";
+            btnColor7.Size = new Size(35, 30);
+            btnColor7.TabIndex = 7;
+            btnColor7.UseVisualStyleBackColor = false;
             // 
-            // btnColor1
+            // btnColor6
             // 
-            btnColor1.BackColor = Color.White;
-            btnColor1.Location = new Point(44, 3);
-            btnColor1.Name = "btnColor1";
-            btnColor1.Size = new Size(35, 30);
-            btnColor1.TabIndex = 1;
-            btnColor1.UseVisualStyleBackColor = false;
-            // 
-            // btnColor2
-            // 
-            btnColor2.BackColor = Color.Red;
-            btnColor2.Location = new Point(85, 3);
-            btnColor2.Name = "btnColor2";
-            btnColor2.Size = new Size(35, 30);
-            btnColor2.TabIndex = 2;
-            btnColor2.UseVisualStyleBackColor = false;
-            // 
-            // btnColor3
-            // 
-            btnColor3.BackColor = Color.Green;
-            btnColor3.Location = new Point(126, 3);
-            btnColor3.Name = "btnColor3";
-            btnColor3.Size = new Size(35, 30);
-            btnColor3.TabIndex = 3;
-            btnColor3.UseVisualStyleBackColor = false;
-            // 
-            // btnColor4
-            // 
-            btnColor4.BackColor = Color.Blue;
-            btnColor4.Location = new Point(167, 3);
-            btnColor4.Name = "btnColor4";
-            btnColor4.Size = new Size(35, 30);
-            btnColor4.TabIndex = 4;
-            btnColor4.UseVisualStyleBackColor = false;
+            btnColor6.BackColor = Color.Magenta;
+            btnColor6.Location = new Point(249, 3);
+            btnColor6.Name = "btnColor6";
+            btnColor6.Size = new Size(35, 30);
+            btnColor6.TabIndex = 6;
+            btnColor6.UseVisualStyleBackColor = false;
             // 
             // btnColor5
             // 
@@ -196,23 +170,51 @@
             btnColor5.UseVisualStyleBackColor = false;
             btnColor5.Click += btnColor5_Click;
             // 
-            // btnColor6
+            // btnColor4
             // 
-            btnColor6.BackColor = Color.Magenta;
-            btnColor6.Location = new Point(249, 3);
-            btnColor6.Name = "btnColor6";
-            btnColor6.Size = new Size(35, 30);
-            btnColor6.TabIndex = 6;
-            btnColor6.UseVisualStyleBackColor = false;
+            btnColor4.BackColor = Color.Blue;
+            btnColor4.Location = new Point(167, 3);
+            btnColor4.Name = "btnColor4";
+            btnColor4.Size = new Size(35, 30);
+            btnColor4.TabIndex = 4;
+            btnColor4.UseVisualStyleBackColor = false;
             // 
-            // btnColor7
+            // btnColor3
             // 
-            btnColor7.BackColor = Color.Cyan;
-            btnColor7.Location = new Point(290, 3);
-            btnColor7.Name = "btnColor7";
-            btnColor7.Size = new Size(35, 30);
-            btnColor7.TabIndex = 7;
-            btnColor7.UseVisualStyleBackColor = false;
+            btnColor3.BackColor = Color.Green;
+            btnColor3.Location = new Point(126, 3);
+            btnColor3.Name = "btnColor3";
+            btnColor3.Size = new Size(35, 30);
+            btnColor3.TabIndex = 3;
+            btnColor3.UseVisualStyleBackColor = false;
+            // 
+            // btnColor2
+            // 
+            btnColor2.BackColor = Color.Red;
+            btnColor2.Location = new Point(85, 3);
+            btnColor2.Name = "btnColor2";
+            btnColor2.Size = new Size(35, 30);
+            btnColor2.TabIndex = 2;
+            btnColor2.UseVisualStyleBackColor = false;
+            // 
+            // btnColor1
+            // 
+            btnColor1.BackColor = Color.White;
+            btnColor1.Location = new Point(44, 3);
+            btnColor1.Name = "btnColor1";
+            btnColor1.Size = new Size(35, 30);
+            btnColor1.TabIndex = 1;
+            btnColor1.UseVisualStyleBackColor = false;
+            // 
+            // btnColor0
+            // 
+            btnColor0.BackColor = Color.Black;
+            btnColor0.Location = new Point(3, 3);
+            btnColor0.Name = "btnColor0";
+            btnColor0.Size = new Size(35, 30);
+            btnColor0.TabIndex = 0;
+            btnColor0.UseVisualStyleBackColor = false;
+            btnColor0.Click += button1_Click;
             // 
             // lblStatus
             // 
@@ -277,5 +279,6 @@
         private Button btnColor4;
         private Label lblStatus;
         private Label lblCooldown;
+        private System.Windows.Forms.Timer coldownTimer;
     }
 }
