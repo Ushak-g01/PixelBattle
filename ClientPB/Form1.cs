d2471bba15821b56aad80bada92b8a45a4ca4dbf
Ushak-g01 <UshakovM190110@yandex.ru>
2026-05-28 18:02:44 +0300

    Код формы

--- ClientPB/Form1.cs
+++ ClientPB/Form1.cs
@@ -1,7 +1,21 @@
+using System.Net.Sockets;
+using Common;
+
 namespace ClientPB
 {
     public partial class Form1 : Form
     {
+        private TcpClient _client;
+        private StreamReader _reader;
+        private StreamWriter _writer;
+        private string _clientId;
+        private int _currentWorldId = -1;
+        private WorldState _currentWorld;
+        private int _selectedColor = 1;
+        private int _cellSize = 10;
+        private DateTime _lastPixelTime = DateTime.MinValue;
+        private Dictionary<int, string> _availableWorlds = new Dictionary<int, string>();
+
         public Form1()
         {
             InitializeComponent();
@@ -26,5 +40,11 @@ namespace ClientPB
         {
 
         }
+    }
+    class WorldItem
+    {
+        public int Id {  get; set; }
+        public string Name { get; set; }
+        public override string ToString() => Name;
     }
 }
