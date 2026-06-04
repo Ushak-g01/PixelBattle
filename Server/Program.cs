11a1296089fe063b209d96b01ff80424ddf882f9
Ushak-g01 <UshakovM190110@yandex.ru>
2026-06-04 18:11:47 +0300

    Добавил ввод для IP-адресов для подключения к серверу

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Server;
+using System.Net;
+var server = new GameServer();
+
+Console.WriteLine("Сервер запущен. Доступные IP для подключения:");
+foreach (var ip in Dns.GetHostEntry(Dns.GetHostName()).AddressList)
+{
+    if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+        Console.WriteLine($" {ip}");
+}
+server.Start();
